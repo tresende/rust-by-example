21ec5b7c5bc19dd400b78bdf84745424ea020736
Thiago Resende <thiago.gcresende@gmail.com>
2022-02-26 02:35:03 +0000

    feat: variables

---
+++ src/ex1.rs
@@ -0,0 +1,11 @@
+//
+
+fn main() {
+    let name = "Thiago";
+    let age = 28;
+    println!(
+        "my name is {name} and i am 20 years {age}",
+        name = name,
+        age = age
+    )
+}
