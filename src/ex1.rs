caaf6f187248acc42aa73fcac6abf4a505903346
Thiago Resende <thiago.gcresende@gmail.com>
2022-02-26 02:43:41 +0000

    feat: fns

--- src/ex1.rs
+++ src/ex1.rs
@@ -1,11 +1,19 @@
 //
 
-fn main() {
-    let name = "Thiago";
-    let age = 28;
+fn sum(x: u64, y: u64) -> u64 {
+    x + y
+}
+
+fn hello(name: String, age: u64) {
     println!(
-        "my name is {name} and i am 20 years {age}",
+        "my name is {name} and i am {age} years ",
         name = name,
         age = age
     )
+}
+
+fn main() {
+    let name = String::from("Thiago");
+    let age = sum(10, 18);
+    hello(name, age)
 }
